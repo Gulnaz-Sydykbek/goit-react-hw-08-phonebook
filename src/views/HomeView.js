ae656a5f73e8d43843a5dbe2088695fd2f4932f1
Gulnaz Sydykbek <75367433+Gulnaz-Sydykbek@users.noreply.github.com>
2021-09-27 22:05:57 +0600

    hw-15

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,7 +1,9 @@
+import s from './View.module.css';
+
 function HomeView() {
   return (
-    <div>
-      <h1>Welkom to the phonebook!</h1>
+    <div className={s.main}>
+      <h1 className={s.welkom}>Welkom to the phonebook!</h1>
     </div>
   );
 }
